4c4cc01176ce202a7e20366d896ca6b1f0339a10
Aleksei Limanskii <AELimanskiy@sberbank.ru>
2023-01-29 15:37:40 +0300

    check

--- src/main/kotlin/line/stockmoex/controller/MoexController.kt
+++ src/main/kotlin/line/stockmoex/controller/MoexController.kt
@@ -8,7 +8,7 @@ import line.stockmoex.model.TickerRequest
 import line.stockmoex.service.StockService
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
-//ccc
+//ccc ffff
 @RestController
 @RequestMapping(path = ["\${stock.endpoint.adapterBase}"], produces = [APPLICATION_JSON_VALUE])
 class MoexController(
